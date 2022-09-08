4b65455e928be020feb9c9353093a731abbb9a5d
anagha bhople <anagha@anaghas-MacBook-Air.local>
2022-09-08 20:40:44 +0530

    change package name and .gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ REQUIREMENTS = ['numpy','pandas','matplotlib','six','plotly',
 'statsmodels','seaborn','sklearn', 'setuptools']
 
 # calling the setup function
-setup(name='primary data analysis',
+setup(name='primarydataanalysis',
 	version='1.0.0',
 	description='Primary data analysis for pandas dataframe',
 	long_description='Primary data analysis for pandas dataframe',
