6aae631330f79ef894275e596624dfdf42eb90d1
anagha bhople <anagha@anaghas-MacBook-Air.local>
2022-09-08 20:27:02 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+REQUIREMENTS = ['numpy','pandas','matplotlib','six','plotly',
+'statsmodels','seaborn','sklearn', 'setuptools']
+
+# calling the setup function
+setup(name='primary data analysis',
+	version='1.0.0',
+	description='Primary data analysis for pandas dataframe',
+	long_description='Primary data analysis for pandas dataframe',
+	url='https://github.com/anagha-bhople/primary_data_analysis',
+	author='Anagha Bhople',
+	author_email='bhoplea34@gmail.com',
+	license='MIT',
+	install_requires=REQUIREMENTS,
+	keywords='data analysis eda pandas'
+	)
